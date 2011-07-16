ddbc93ca0ff37cfa582c27bb277975ffbe3dd2a9
Danny Tuppeny <danny@tuppeny.com>
2011-07-16 11:49:00 +0100

    Removed Form Border.

--- G+ Notifier/MainForm.Designer.cs
+++ G+ Notifier/MainForm.Designer.cs
@@ -33,6 +33,7 @@
 			this.notificationIcon = new System.Windows.Forms.NotifyIcon(this.components);
 			this.notificatinIconMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
 			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.label1 = new System.Windows.Forms.Label();
 			this.notificatinIconMenu.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -58,11 +59,24 @@
 			this.exitToolStripMenuItem.Text = "E&xit";
 			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
 			// 
+			// label1
+			// 
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(284, 262);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "// TODO: Me";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "MainForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "MainForm";
@@ -79,5 +93,6 @@
 		private System.Windows.Forms.NotifyIcon notificationIcon;
 		private System.Windows.Forms.ContextMenuStrip notificatinIconMenu;
 		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.Label label1;
 	}
 }
