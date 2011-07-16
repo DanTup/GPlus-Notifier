2bfe5f78e087546cd70e22a3560a99f7ae558de8
Danny Tuppeny <danny@tuppeny.com>
2011-07-16 11:20:12 +0100

    Set default namespaces, tidied up AssemblyInfo etc.

--- G+ Notifier/MainForm.Designer.cs
+++ G+ Notifier/MainForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace G__Notifier
+﻿namespace DanTup.GPlusNotifier
 {
-	partial class Form1
+	partial class MainForm
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -30,10 +30,9 @@
 		{
 			this.components = new System.ComponentModel.Container();
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.Text = "MainForm";
 		}
 
 		#endregion
 	}
 }
-
