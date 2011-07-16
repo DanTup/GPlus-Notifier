b0e4608d862520f3abbb398dacd4f32d2880b8e1
Danny Tuppeny <danny@tuppeny.com>
2011-07-16 11:45:40 +0100

    Added basic form (hidden at startup) with NotifyIcon and ability to open/exit.

--- G+ Notifier/MainForm.Designer.cs
+++ G+ Notifier/MainForm.Designer.cs
@@ -29,10 +29,55 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.notificationIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.notificatinIconMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.notificatinIconMenu.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notificationIcon
+			// 
+			this.notificationIcon.ContextMenuStrip = this.notificatinIconMenu;
+			this.notificationIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notificationIcon.Icon")));
+			this.notificationIcon.Text = "G+ Notifier";
+			this.notificationIcon.Visible = true;
+			this.notificationIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notificationIcon_MouseDoubleClick);
+			// 
+			// notificatinIconMenu
+			// 
+			this.notificatinIconMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+			this.notificatinIconMenu.Name = "notificatinIconMenu";
+			this.notificatinIconMenu.Size = new System.Drawing.Size(93, 26);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+			this.exitToolStripMenuItem.Text = "E&xit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "MainForm";
+			this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.notificatinIconMenu.ResumeLayout(false);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.NotifyIcon notificationIcon;
+		private System.Windows.Forms.ContextMenuStrip notificatinIconMenu;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
 	}
 }
--- G+ Notifier/MainForm.cs
+++ G+ Notifier/MainForm.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace DanTup.GPlusNotifier
@@ -14,6 +8,40 @@ namespace DanTup.GPlusNotifier
 		public MainForm()
 		{
 			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			// We only want to show in the notification area at startup, so hide the form.
+			this.Hide();
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			// HACK: The systray icons stays visible if we just quit the app, so remove it first
+			//niSystray.Visible = false;
+			Application.Exit();
+		}
+
+		private void notificationIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			// Show the notification window
+			this.Show();
+			this.WindowState = FormWindowState.Normal;
+			this.BringToFront();
+		}
+
+		private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			// HACK: Hijack closing, and just minimise instead. This is because this form is actually what's showing the
+			// notification icon and if we close the form, the icon will disappear. Is there a better way of having a
+			// NotifyIcon without a form?
+			if (e.CloseReason == CloseReason.UserClosing)
+			{
+				e.Cancel = true;
+				this.Hide();
+				this.WindowState = FormWindowState.Minimized;
+			}
 		}
 	}
 }
